cabb7e2437de3858271f7405f1c0af7cf9359f0f
hye <hye@DESKTOP-EFRL1C1>
2020-08-31 14:51:26 +0900

    추천 셀렉트

--- src/main/java/com/wit/main/controller/MainController.java
+++ src/main/java/com/wit/main/controller/MainController.java
@@ -1,5 +1,9 @@
 package com.wit.main.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.wit.custom.controller.CustomController;
 import com.wit.main.dao.MainDAO;
+import com.wit.main.dto.MainDTO;
 
 @Controller
 @RequestMapping(value = "/main")
@@ -20,11 +25,33 @@ public class MainController {
 	
 	private Logger log = LoggerFactory.getLogger(CustomController.class);
 	
-	@RequestMapping(value = "/main", method = RequestMethod.GET)
-	public String main() {		
+	/*
+	 * @RequestMapping(value = "/main", method = {RequestMethod.GET,
+	 * RequestMethod.POST}) public String main(HttpServletRequest req) {
+	 * 
+	 * //타일즈/내가 설정하는 jsp경로 return ".tiles/views/index"; }
+	 */
+	
+	@RequestMapping(value = "/main", method = {RequestMethod.GET, RequestMethod.POST})
+	public String main(HttpServletRequest req,MainDTO dto) {	
 		
-		//타일즈/내가 설정하는 jsp경로
+		dto.setUser_Id("user");
+		dto.setUser_Style("CAS");
+		
+		if(dto.getUser_Id()!=null || !dto.getUser_Id().equals("")) {
+			
+			List<MainDTO> lists = dao.selectUserStyle(dto.getUser_Id(), dto.getUser_Style());
+			req.setAttribute("lists", lists);
+			return ".tiles/views/index";
+		}else {
+			
+		//로그인 안됐을시 별점순 정렬
+		
 		return ".tiles/views/index";
+		
+		
 	}
 
+}
+	
 }
